ecb8bcd03e00bcdbcb2a544f5994b10520f05e8e
renactemotion <jenna.jaeyeon@gmail.com>
2021-08-19 08:27:57 +0900

    drawing line

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
 
-function onMouseMove(event){
+canvas.width = 500;
+canvas.height = 500;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2;
+
+let painting = false;
+
+function stopPainting() {
+    painting = false;
+}
+
+function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
-    console.log(x, y);
+    if (!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
+function onMousedown(event) {
+    painting = true;
+}
+
+function onMouseUp(event) {
+    stopPainting()
 }
 
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", onMousedown);
+    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mouseleave", stopPainting);
 }
